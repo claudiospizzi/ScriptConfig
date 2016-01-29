f4fbe8dcadd338ce701881f839698575b1d6644f
Claudio Spizzi <claudio.spizzi@live.com>
2016-01-29 16:45:38 +0100

    Update the demo scripts for better visualisation

--- Examples/XmlConfigDemo.ps1
+++ Examples/XmlConfigDemo.ps1
@@ -8,5 +8,9 @@ Write-Host "Integer Positive :" $Config.MyIntegerPositive
 Write-Host "Integer Negative :" $Config.MyIntegerNegative
 Write-Host "Boolean True     :" $Config.MyBooleanTrue
 Write-Host "Boolean False    :" $Config.MyBooleanFalse
-Write-Host "Array            :" "@(" (($Config.MyArray | ForEach-Object { '"{0}"' -f $_ }) -join ', ') ")"
-Write-Host "Hashtable        :" "@{" (($Config.MyHashtable.GetEnumerator() | ForEach-Object { '{0} = "{1}"' -f $_.Name, $_.Value }) -join '; ') "}"
+Write-Host "Array            :" $Config.MyArray
+Write-Host "Array Item       :" $Config.MyArray[0]
+Write-Host "Hashtable        :" $Config.MyHashtable
+Write-Host "Hashtable Item   :" $Config.MyHashtable['Hello']
+Write-Host "Hashtable Keys   :" $Config.MyHashtable.Keys
+Write-Host "Hashtable Values :" $Config.MyHashtable.Values
