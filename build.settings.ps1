bb5e1f5607ae9976e2ca43109b21957fe6e8bc31
Claudio Spizzi <claudio.spizzi@arcade.ch>
2019-09-16 21:33:48 +0200

    Update build script

--- build.settings.ps1
+++ build.settings.ps1
@@ -4,9 +4,9 @@ Properties {
     $ModuleNames    = 'ScriptConfig'
 
     $GalleryEnabled = $true
-    $GalleryKey     = Use-VaultSecureString -TargetName 'PowerShell Gallery Key'
+    $GalleryKey     = Use-VaultSecureString -TargetName 'PowerShell Gallery Key (claudiospizzi)'
 
     $GitHubEnabled  = $true
     $GitHubRepoName = 'claudiospizzi/ScriptConfig'
-    $GitHubToken    = Use-VaultSecureString -TargetName 'GitHub Token'
+    $GitHubToken    = Use-VaultSecureString -TargetName 'GitHub Token (claudiospizzi)'
 }
